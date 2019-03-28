a77946e8c6fca0fb07fc99709a88a209bef47939
kharioki <tony.kharioki@gmail.com>
2019-03-28 18:45:05 +0300

    firebase hosting deploy

---
+++ src/components/PasswordChange/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PasswordChange extends Component {
+  render() {
+    return (
+      <div>
+        <h1>PasswordChange</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/Session/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Session extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Session</h1>
+      </div>
+    );
+  }
+}
